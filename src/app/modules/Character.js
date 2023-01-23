8d6a99a90ff122a82e026551e988971629f6b5ed
Turovae <ekaterina.turova@mail.ru>
2023-01-23 16:08:47 +0700

    add getRandomValue to Character class

--- src/app/modules/Character.js
+++ src/app/modules/Character.js
@@ -1,10 +1,14 @@
+function getRandomValue() {
+  return Math.floor(Math.random() * 5 + 1) * 10;
+}
+
 export default class Character {
   constructor(name, type) {
     this.name = name;
     this.type = type;
     this.health = 100;
     this.level = 1;
-    this.attack = 40;
-    this.defence = 20;
+    this.attack = getRandomValue();
+    this.defence = getRandomValue();
   }
 }
